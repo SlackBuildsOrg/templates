6f5c3aaee7279b07279389898d02a20f8bfa363a
Robby Workman <rworkman@slackbuilds.org>
2010-06-18 17:18:02 -0500

    doinst.sh: Add schema_install() example for GConf stuff

--- doinst.sh
+++ doinst.sh
@@ -22,6 +22,15 @@ preserve_perms() {
   config $NEW
 }
 
+schema_install() {
+  SCHEMA="$1"
+  GCONF_CONFIG_SOURCE="xml::etc/gconf/gconf.xml.defaults" \
+  chroot . gconftool-2 --makefile-install-rule \
+    /etc/gconf/schemas/$SCHEMA \
+    1>/dev/null
+}
+
+schema_install blah.schemas
 preserve_perms etc/rc.d/rc.INIT.new
 config etc/configfile.new
 
