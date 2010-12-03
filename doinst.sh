5d835f08aa4bbc93a5c59947270d99577094eeab
Robby Workman <rworkman@slackbuilds.org>
2010-12-02 23:38:28 -0600

    doinst.sh: compile glib2 schemas if needed
    This is not really relevant for us yet, but it won't hurt.

--- doinst.sh
+++ doinst.sh
@@ -48,3 +48,7 @@ if [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
   fi
 fi
 
+if [ -x /usr/bin/glib-compile-schemas ]; then
+  /usr/bin/glib-compile-schemas /usr/share/glib-2.0/schemas/
+fi
+
