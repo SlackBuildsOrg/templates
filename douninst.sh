2dbac016f564b0fe57019b56c2b1b176b4c3ebe9
Willy Sudiarto Raharjo <willysr@slackbuilds.org>
2021-07-08 23:44:41 +0700

    Added douninst.sh.
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

---
+++ douninst.sh
@@ -0,0 +1,6 @@
+# douninst.sh
+#
+# uninstall script for Slackware >= 15.0
+#
+# NOTE: This script is run AFTER package removal, so be careful!
+#       Consider it optional, use if it is really needed.
